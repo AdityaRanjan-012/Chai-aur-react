940c171361a6a9d1fd93cad39c8e78aed5486a0c
user.email <adityaranjan3003@gmail.com>
2025-05-10 17:51:04 +0530

    Added 01basicreact folder

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Chai from './chai';
+function App() {
+  return (
+    <div className="App">
+      <Chai />
+      <h1>Create React App</h1>
+      <p>This is a basic React application.</p>
+    </div>
+  );
+}
+
+export default App;
