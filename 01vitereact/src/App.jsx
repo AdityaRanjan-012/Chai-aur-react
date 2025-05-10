6503f77e056ff664c03cbc8a5bc21f6da598daa7
user.email <adityaranjan3003@gmail.com>
2025-05-10 17:59:50 +0530

    Added 01basicreact and 01vitereact folder

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Chai from './Chai.jsx'
+function App() {
+  
+
+  return (
+    <>
+    <Chai/>
+    <h1>Vite </h1>
+    <p>This is a basic React application with vite.</p>
+    </>
+  )
+}
+
+export default App
